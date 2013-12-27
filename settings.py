982f24e5364c39696be79cedf4a81ad0c3cb8842
Frol <frol@ewadev.com>
2013-12-27 08:43:51 +0200

    add ALLOWED_HOSTS

--- settings.py
+++ settings.py
@@ -18,6 +18,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
